423e2c21a9896a1626f54a7996643bf63fb09000
JoaoCosta94 <jpcc0394@gmail.com>
2015-07-08 18:42:16 +0100

    Arrays of complex numbers are working

--- Bloch Goes to Nonlinear Schrodinger/Test Code/test with kernel.py
+++ Bloch Goes to Nonlinear Schrodinger/Test Code/test with kernel.py
@@ -17,7 +17,6 @@ Y2_h = np.array([4 + 1j*5, 5 + 1j*6, 6 + 1j*7]).astype(np.complex64)
 Y3_h = np.array([4 + 1j*5, 5 + 1j*6, 6 + 1j*7]).astype(np.complex64)
 aux_h = np.complex64(1 + 1j*1)
 RES_h = np.empty_like(X1_h)
-id_h = np.empty_like(X1_h)
 
 dados_h = []
 for i in range(3):
@@ -29,26 +28,18 @@ print dados_h
 aux_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=aux_h)
 dados_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=dados_h)
 RES_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf = RES_h)
-id_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf = id_h)
-
 
 Source = """
-__kernel void soma( __global float8 *dados, __global float2 *id, __global float2 *res){
+__kernel void soma( __global float2 *dados, __global float2 *res, int rowWidth){
 	const int gid_x = get_global_id(0);
-	const int gid_y = get_global_id(1);
-	id[gid_x].x = gid_x;
-	id[gid_x].y = gid_y;
-	res[gid_x] = dados[gid_x];
+	res[gid_x] = dados[gid_x*rowWidth+3];
 }
 """
 prg = cl.Program(ctx, Source).build()
 
-completeEvent = prg.soma(queue, (M,), None, dados_d, id_d, RES_d)
+completeEvent = prg.soma(queue, (M,), None, dados_d, RES_d, np.int32(6))
 completeEvent.wait()
 
 cl.enqueue_copy(queue, RES_h, RES_d)
-cl.enqueue_copy(queue, id_h, id_d)
-print "GPU ID"
-print id_h
 print "GPU RES"
 print RES_h
--- Bloch Goes to Nonlinear Schrodinger/Test Code/test with kernel_random_addition.py
+++ Bloch Goes to Nonlinear Schrodinger/Test Code/test with kernel_random_addition.py
@@ -14,33 +14,43 @@ P33_h = (np.random.randn(M) + 1j*np.random.randn(M)).astype(np.complex64)
 P21_h = (np.random.randn(M) + 1j*np.random.randn(M)).astype(np.complex64)
 P31_h = (np.random.randn(M) + 1j*np.random.randn(M)).astype(np.complex64)
 P32_h = (np.random.randn(M) + 1j*np.random.randn(M)).astype(np.complex64)
-aux_h = np.complex64(1 + 1j*1)
-RES_h = np.empty_like(P11_h)
 
 dados_h = []
+
 for i in range(3):
-      a = np.array([P11_h[i], P22_h[i], P33_h[i], P21_h[i], P31_h[i], P32_h[i]]).astype(np.complex64)
+      #a = np.array([P11_h[i], P22_h[i], P33_h[i], P21_h[i], P31_h[i], P32_h[i]]).astype(np.complex64)
+      a = np.array([P11_h[i], P22_h[i]]).astype(np.complex64)
       dados_h.append(a)
+
+dados2_h = dados_h[:]
 dados_h = np.array(dados_h).astype(np.complex64)
+dados2_h = np.array(dados2_h).astype(np.complex64)
+RES_h = np.empty_like(dados_h)
 
+print "dados1"
 print dados_h
+print "\n Expected"
+print dados_h * 2
 
-aux_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=aux_h)
 dados_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=dados_h)
+dados2_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf=dados2_h)
 RES_d = cl.Buffer(ctx, MF.READ_WRITE | MF.COPY_HOST_PTR, hostbuf = RES_h)
 
 
 Source = """
-__kernel void soma(__global float2 *aux, __global float2 *dados, __global float2 *res, int W){
+__kernel void soma(__global float2 *dados, __global float2 *dados2, __global float2 *res, int W){
 	const int gid_x = get_global_id(0);
-	res[gid_x] = dados[gid_x*W+1] * 2;
+	for(int i = 0; i<W; i++)
+	{
+             res[gid_x*W+i] = dados[gid_x*W+i] + dados2[gid_x*W+i];
+	}
 }
 """
 prg = cl.Program(ctx, Source).build()
 
-completeEvent = prg.soma(queue, (M,), None, aux_d, dados_d, RES_d, np.int32(6))
+completeEvent = prg.soma(queue, (M,), None, dados_d, dados2_d, RES_d, np.int32(2))
 completeEvent.wait()
 
 cl.enqueue_copy(queue, RES_h, RES_d)
-print "GPU RES"
-print RES_h
+print "\n RES"
+print RES_h - (dados_h + dados2_h)
